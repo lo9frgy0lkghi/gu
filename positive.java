5efe197c45437de98226ae592687f0ef4b6de423
lo9frgy0lkghi <47377205+lo9frgy0lkghi@users.noreply.github.com>
2019-02-06 14:53:26 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class ideone
+{
+	public static void main(String[]args)
+	{
+		int a;
+		Scanner S=new Scanner(System.in);
+		System.out.println("");
+		a=S.nextInt();
+		if(a>0)
+		{
+			System.out.println("Positive");
+		}
+		else if(a==0)
+		{
+			System.out.println("Negative");
+		}
+	}
+	
+}	
+	
